93300e767b4b385b1edb8899ecbe6c6a5de5d51b
PCBsystem <pcbsytem@hotmail.com>
2017-04-16 17:34:40 -0300

    remoção de alguns arquivos que foram colocados no local errado.

--- AjudaController.js
+++
@@ -1,9 +0,0 @@
-angular.module('contatooh')
-    .controller('AjudaController', 
-    function($scope, $resource, $http) {        
-         $http.get('/ajuda')
-         .then(function(res) {
-            $scope.topicos = res.data;
-         });       
-    }
-);
