02c35c4c8d6fc0fb33be841108449b1e02f98416
Thiago Scalone <thiago@scalone.com.br>
2017-04-16 22:44:04 -0300

    Add support for contact phone field.

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -2,17 +2,20 @@ var contatos = [
    {
       _id: 1,
       nome: 'Contato Exemplo 1',
-      email: 'cont1@empresa.com.br'
+      email: 'cont1@empresa.com.br',
+      phone: 992410586
    },
    {
       _id: 2,
       nome: 'Contato Exemplo 2',
-      email: 'cont2@empresa.com.br'
+      email: 'cont2@empresa.com.br',
+      phone: 992410587
    },
    {
       _id: 3,
       nome: 'Contato Exemplo 3',
-      email: 'cont3@empresa.com.br'
+      email: 'cont3@empresa.com.br',
+      phone: 992410588
    }
 ];
 
@@ -90,3 +93,4 @@ module.exports = function() {
 
    return controller;
 };
+
