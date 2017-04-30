fb3025aeb64ffc574304e5eaf832a70c1249c2f3
fgcintra <fausto@fgcintra.pro.br>
2017-04-30 18:20:02 -0300

    Revert "Trabalho de Tópicos Especiais"

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -2,20 +2,17 @@ var contatos = [
    {
       _id: 1,
       nome: 'Contato Exemplo 1',
-      email: 'cont1@empresa.com.br',
-      fone: 1637274325
+      email: 'cont1@empresa.com.br'
    },
    {
       _id: 2,
       nome: 'Contato Exemplo 2',
-      email: 'cont2@empresa.com.br',
-      fone: 1637076200
+      email: 'cont2@empresa.com.br'
    },
    {
       _id: 3,
       nome: 'Contato Exemplo 3',
-      email: 'cont3@empresa.com.br',
-      fone: 1637205907
+      email: 'cont3@empresa.com.br'
    }
 ];
 
