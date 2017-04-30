8a202970b54a8f0fe8329422e1f1c2da33c5cb4c
fgcintra <fausto@fgcintra.pro.br>
2017-04-30 18:55:36 -0300

    Revert "Trabalho 1 Alan Jones Faria"

--- app/routes/ajuda.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-   
-   var controller = app.controllers.ajuda;
-   
-   //app.get('/contatos', controller.listaContatos);
-   app.route('/ajuda')
-      .get(controller.listaAjuda);
-};
