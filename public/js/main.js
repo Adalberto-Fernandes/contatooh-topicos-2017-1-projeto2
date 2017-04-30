c556b365dd08805ce8980861f0e7ce151ed6a119
fgcintra <fausto@fgcintra.pro.br>
2017-04-30 18:24:49 -0300

    Revert "Trabalho 1 Guilherme Naldi Stefanelli"

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,21 @@
 angular.module('contatooh', ['ngRoute', 'ngResource'])
-      .config(function ($routeProvider) {
-
-            $routeProvider.when('/contatos', {
-                  templateUrl: 'partials/contatos.html',
-                  controller: 'ContatosController'
-            });
-
-            $routeProvider.when('/contato/:contatoId', {
-                  templateUrl: 'partials/contato.html',
-                  controller: 'ContatoController'
-            });
-
-            $routeProvider.when('/contato', {
-                  templateUrl: 'partials/contato.html',
-                  controller: 'ContatoController'
-            });
-
-            $routeProvider.when('/ajuda', {
-                  templateUrl: 'partials/ajuda.html',
-                  controller: 'AjudaController'
-            });
-
-            $routeProvider.otherwise({ redirectTo: '/contatos' });
-
+   .config(function($routeProvider) {
+      
+      $routeProvider.when('/contatos', {
+         templateUrl: 'partials/contatos.html',
+         controller: 'ContatosController'
       });
+
+      $routeProvider.when('/contato/:contatoId', {
+         templateUrl: 'partials/contato.html',
+         controller: 'ContatoController'
+      });
+
+      $routeProvider.when('/contato', {
+         templateUrl: 'partials/contato.html',
+         controller: 'ContatoController'
+      });
+
+      $routeProvider.otherwise({redirectTo: '/contatos'});
+   
+});
