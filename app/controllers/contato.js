b0faca14ae511a5964a72fba0e40ae21970a7455
fgcintra <fausto@fgcintra.pro.br>
2017-05-02 09:35:58 -0300

    Revert "Trabalho Aline e Danilo - Tópicos especiais de informática - contatooh"

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -2,20 +2,17 @@ var contatos = [
    {
       _id: 1,
       nome: 'Contato Exemplo 1',
-      email: 'cont1@empresa.com.br',
-      Telefone :  16999999999
+      email: 'cont1@empresa.com.br'
    },
    {
       _id: 2,
       nome: 'Contato Exemplo 2',
-      email: 'cont2@empresa.com.br',
-      Telefone :  16988888888
+      email: 'cont2@empresa.com.br'
    },
    {
       _id: 3,
       nome: 'Contato Exemplo 3',
-      email: 'cont3@empresa.com.br',
-      Telefone :  1637111111
+      email: 'cont3@empresa.com.br'
    }
 ];
 
